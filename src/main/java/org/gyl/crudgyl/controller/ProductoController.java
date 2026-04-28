63f9e6309a0dc1bfaab95b68e7c53b224ccb0536
JPaulRamirez <p.ramirez9991@gmail.com>
2026-04-28 09:16:27 -0300

    Primer commit

---
+++ src/main/java/org/gyl/crudgyl/controller/ProductoController.java
@@ -0,0 +1,58 @@
+package org.gyl.crudgyl.controller;
+
+import jakarta.validation.Valid;
+import org.gyl.crudgyl.dto.ProductoRequestDTO;
+import org.gyl.crudgyl.dto.ProductoResponseDTO;
+import org.gyl.crudgyl.service.ProductoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/productos")
+
+public class ProductoController {
+
+    private final ProductoService productoService;
+
+    public ProductoController(ProductoService productoService)
+    {
+        this.productoService = productoService;
+    }
+
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ProductoResponseDTO crear(@Valid @RequestBody ProductoRequestDTO dto){
+        return productoService.crear(dto);
+
+    }
+
+    @GetMapping
+    public List<ProductoResponseDTO> listar() {
+        return productoService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public ProductoResponseDTO buscarPorIdDto(@PathVariable Long id)
+    {
+        return productoService.buscarPorID(id);
+    }
+    @PutMapping("/{id}")
+    public ProductoResponseDTO actualizar(@PathVariable Long id,@RequestBody ProductoRequestDTO dto)
+    {
+        return productoService.actualizar(id,dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id)
+    {
+        productoService.eliminar(id);
+    }
+    @GetMapping("/buscar/{nombre}")
+    public List<ProductoResponseDTO> busquedaNombre(@PathVariable String nombre) {
+        return productoService.busquedaNombre(nombre);
+    }
+
+}
---
+++ src/main/java/org/gyl/crudgyl/service/ProductoService.java
@@ -0,0 +1,20 @@
+package org.gyl.crudgyl.service;
+
+import org.gyl.crudgyl.dto.ProductoRequestDTO;
+import org.gyl.crudgyl.dto.ProductoResponseDTO;
+
+import java.util.List;
+
+public interface ProductoService {
+
+    ProductoResponseDTO crear(ProductoRequestDTO producto);
+
+    List<ProductoResponseDTO> listar();
+
+    ProductoResponseDTO buscarPorID(Long id);
+
+    ProductoResponseDTO actualizar(Long id , ProductoRequestDTO dto);
+    void eliminar(Long id);
+
+    List<ProductoResponseDTO> busquedaNombre(String nombre);
+}
