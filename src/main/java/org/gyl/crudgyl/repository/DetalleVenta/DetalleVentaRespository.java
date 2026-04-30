3abb17c358b2eb0058e7eaa75f8bdc248efcbd51
JPaulRamirez <p.ramirez9991@gmail.com>
2026-04-30 15:15:14 -0300

    Fix/3004/Arreglando detalleVenta Repository

--- src/main/java/org/gyl/crudgyl/repository/DetalleVenta/DetalleVentaRespository.java
+++
@@ -1,9 +0,0 @@
-package org.gyl.crudgyl.repository.DetalleVenta;
-
-import org.gyl.crudgyl.entity.DetalleVenta.DetalleVenta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DetalleVentaRepository extends JpaRepository<DetalleVenta, Long> {
-}
