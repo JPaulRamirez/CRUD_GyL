6e69754004d1547fd98a9dd9a7451197dba2b8a2
JPaulRamirez <p.ramirez9991@gmail.com>
2026-05-04 17:24:36 -0300

    Feature/agregue manejar validaciones

--- src/main/java/org/gyl/crudgyl/exception/GlobalExceptionHandler.java
+++ src/main/java/org/gyl/crudgyl/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package org.gyl.crudgyl.exception;
 import org.gyl.crudgyl.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,5 +17,23 @@ public class GlobalExceptionHandler {
     {
         ErrorResponse error = new ErrorResponse(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), "Recurso no encontrado", ex.getMessage());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> manejarValidaciones(MethodArgumentNotValidException ex) {
+        String mensaje = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .findFirst()
+                .orElse("Error de validación");
+
+        ErrorResponse error = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Datos inválidos",
+                mensaje
+        );
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
